f1e7606f93743b3efad572b7ce3b8ec2ff580d23
mawen <mawen@meicloud.com>
2019-09-23 10:25:07 +0800

    add the interface to get branches and tags

--- src/main/java/com/GitClient/GitClient.java
+++ src/main/java/com/GitClient/GitClient.java
@@ -14,8 +14,8 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 import java.io.Serializable;
-import java.util.Collection;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GitClient implements Serializable {
 
@@ -34,6 +34,13 @@ public class GitClient implements Serializable {
 
     private RepoCredit repoCredit;
 
+    /**
+     * if you want to get commitId, use this constructor to initialize gitClient
+     *
+     * @param url               is repository's http or ssh url
+     * @param originalReference is repository's branchName、tagName or commitId
+     * @param repoCredit        is repository's credential,two ways like username-password or ssh private key
+     */
     public GitClient(String url, String originalReference, RepoCredit repoCredit) {
         if (StringUtils.isEmpty(url) || StringUtils.isEmpty(originalReference) || ObjectUtils.isEmpty(repoCredit) || StringUtils.isEmpty(repoCredit.getUsername())) {
             logger.error("new GitlabClient() error.Initialize GitlabClient's parameters can not be null.Url = {}, originalReference = {}", url, originalReference);
@@ -44,8 +51,28 @@ public class GitClient implements Serializable {
         this.repoCredit = repoCredit;
     }
 
+    /**
+     * if you want to get  repository's branchNames and tagNames,use this constructor to initialize gitClient
+     *
+     * @param url        is repository's http or ssh url
+     * @param repoCredit is repository's credential,two ways like username-password or ssh private key
+     */
+    public GitClient(String url, RepoCredit repoCredit) {
+        if (StringUtils.isEmpty(url) || ObjectUtils.isEmpty(repoCredit) || StringUtils.isEmpty(repoCredit.getUsername())) {
+            logger.error("new GitlabClient() error.Initialize GitlabClient's url and repoCredit can not be null.Url = {},", url);
+            throw new GitlabClientException(RetCode.REPO_CREDENTIAL_IS_NULL);
+        }
+        this.url = url;
+        this.repoCredit = repoCredit;
+    }
+
+    /**
+     * get a commitId convert by branchName、tagName or commitId which user input
+     *
+     * @return a commitId
+     */
     public Optional<String> getCommitId() {
-        //http和ssh两种授信方式
+        //http or ssh credential
         if (url.startsWith(HTTP_URL_PREFIX)) {
             return Optional.ofNullable(getCommitIdFinal(generateCredentialsProvider()));
         } else {
@@ -53,22 +80,26 @@ public class GitClient implements Serializable {
         }
     }
 
+    /**
+     * get all the repository's BranchNames and TagNames
+     *
+     * @return a Map<String, List<String>>,include two keys are "branches" and "tags",you can get data through these keys
+     */
+    public Map<String, List<String>> getBranchesAndTags() {
+        if (url.startsWith(HTTP_URL_PREFIX)) {
+            return getBranchesAndTagsFinal(generateCredentialsProvider());
+        } else {
+            return getBranchesAndTagsFinal(generateTransportConfigCallback());
+        }
+    }
+
     private <T> String getCommitIdFinal(T gitCredit) {
         try {
-            Collection<Ref> refCollection = null;
-            if (gitCredit instanceof CredentialsProvider) {
-                refCollection = Git.lsRemoteRepository().setCredentialsProvider((CredentialsProvider) gitCredit).setHeads(true).setTags(true).setRemote(url).call();
-            } else if (gitCredit instanceof TransportConfigCallback) {
-                refCollection = Git.lsRemoteRepository().setTransportConfigCallback((TransportConfigCallback) gitCredit).setHeads(true).setTags(true).setRemote(url).call();
-            }
-            if (refCollection == null) {
-                logger.info("GitlabClient obtained commitId is null. originalReference = {},url = {}username = {}", originalReference, url, repoCredit.getUsername());
-                return null;
-            }
+            Collection<Ref> refCollection = getRefCollection(gitCredit);
             String branch = BRANCH_PREFIX + originalReference;
             String tag = TAG_PREFIX + originalReference;
             for (Ref ref : refCollection) {
-                //不论用户输入git的分支、标签或者commitId，都转换成commitId
+                //no matter user input git's branchName、tagName or commitId, always transfer to commitId and output it
                 if (branch.equals(ref.getName()) || tag.equals(ref.getName()) || originalReference.equals(ref.getObjectId().getName())) {
                     return ref.getObjectId().getName();
                 }
@@ -80,8 +111,43 @@ public class GitClient implements Serializable {
         return null;
     }
 
+    private <T> Map<String, List<String>> getBranchesAndTagsFinal(T gitCredit) {
+        try {
+            Collection<Ref> refCollection = getRefCollection(gitCredit);
+            Map<String, List<String>> result = new HashMap<>();
+            //split all the branchNames and tagNames
+            List<String> branches = refCollection.stream().map(t -> t.getName().split(BRANCH_PREFIX)[1]).sorted().collect(Collectors.toList());
+            List<String> tags = refCollection.stream().map(t -> t.getName().split(TAG_PREFIX)[1]).sorted().collect(Collectors.toList());
+            result.put("branches", branches);
+            result.put("tags", tags);
+            return result;
+        } catch (Exception e) {
+            logger.error("getCommitIdFinal() error. originalReference = {},url = {}username = {}", originalReference, url, repoCredit.getUsername(), e);
+            throw new GitlabClientException(RetCode.GIT_CLIENT_ERROR);
+        }
+    }
+
+    private <T> Collection<Ref> getRefCollection(T gitCredit) {
+        try {
+            Collection<Ref> refCollection = null;
+            if (gitCredit instanceof CredentialsProvider) {
+                refCollection = Git.lsRemoteRepository().setCredentialsProvider((CredentialsProvider) gitCredit).setHeads(true).setTags(true).setRemote(url).call();
+            } else if (gitCredit instanceof TransportConfigCallback) {
+                refCollection = Git.lsRemoteRepository().setTransportConfigCallback((TransportConfigCallback) gitCredit).setHeads(true).setTags(true).setRemote(url).call();
+            }
+            if (refCollection == null) {
+                logger.info("GitlabClient obtained commitId is null. originalReference = {},url = {}username = {}", originalReference, url, repoCredit.getUsername());
+                throw new GitlabClientException(RetCode.GIT_GET_REF_NULL);
+            }
+            return refCollection;
+        } catch (Exception e) {
+            logger.error("getRefCollection() error. originalReference = {},url = {}username = {}", originalReference, url, repoCredit.getUsername(), e);
+            throw new GitlabClientException(RetCode.GIT_GET_REF_ERROR);
+        }
+    }
+
     /**
-     * 用于生成git远程访问所需要的username-password授信
+     * generate UsernamePasswordCredentialsProvider for access git repository remotely by username-password
      */
     private CredentialsProvider generateCredentialsProvider() {
         if (StringUtils.isEmpty(repoCredit.getPassword())) {
@@ -92,7 +158,7 @@ public class GitClient implements Serializable {
     }
 
     /**
-     * 用于生成git远程访问所需要的ssh授信
+     * generate TransportConfig for access git repository remotely by ssh private key
      */
     private TransportConfigCallback generateTransportConfigCallback() {
         if (StringUtils.isEmpty(repoCredit.getSshKey())) {
@@ -102,7 +168,7 @@ public class GitClient implements Serializable {
         SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
             @Override
             protected void configure(OpenSshConfig.Host host, Session session) {
-                //跳过StrictHostKeyChecking，若不跳过，当~/.ssh/known_hosts中没有要访问的域名时会报错
+                //skip StrictHostKeyChecking，if not，it will report error when known_hosts(~/.ssh/known_hosts) file didn't have the domain name which we are about to access
                 session.setConfig("StrictHostKeyChecking", "no");
             }
 
--- src/main/java/com/GitClient/RetCode.java
+++ src/main/java/com/GitClient/RetCode.java
@@ -2,9 +2,11 @@ package com.GitClient;
 
 public enum RetCode {
 
-    MISS_ESSENTIAL_PARAM(218316,"请求参数错误，缺少必要参数"),
-    REPO_CREDENTIAL_IS_NULL(218318,"代码仓库授信为空"),
-    GIT_CLIENT_ERROR(218319,"GitClient获取commitId失败");
+    MISS_ESSENTIAL_PARAM(1,"请求参数错误，缺少必要参数"),
+    REPO_CREDENTIAL_IS_NULL(2,"代码仓库授信为空"),
+    GIT_GET_REF_NULL(3,"GitClient获取RefCollection失败"),
+    GIT_GET_REF_ERROR(4,"GitClient获取RefCollection失败"),
+    GIT_CLIENT_ERROR(5,"GitClient获取commitId失败");
 
     private final int code;
     private final String msg;
